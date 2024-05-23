deb34602f8cd3d750214a0fb7a156863966e10f8
Garrett Leber <garrett@leberra.com>
2024-05-23 16:49:56 -0400

    Improve lumberjack layout

--- keyboards/peej/lumberjack/keymaps/tcpkump/keymap.c
+++ keyboards/peej/lumberjack/keymaps/tcpkump/keymap.c
@@ -27,36 +27,36 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_BASE] = LAYOUT_ortho_5x12(
-    KC_ESC,  KC_1,      KC_2,    KC_3,    KC_4,       KC_5,              KC_6,       KC_7,   KC_8,    KC_9,       KC_0,       KC_BSPC,
-    KC_TAB,  KC_Q,      KC_W,    KC_F,    KC_P,       KC_B,              KC_J,       KC_L,   KC_U,    KC_Y,       KC_QUOT,    KC_DEL,
-    KC_LCTL, KC_A,      KC_R,    KC_S,    KC_T,       KC_G,              KC_M,       KC_N,   KC_E,    KC_I,       KC_O,       KC_ENT,
-    KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_D,       KC_V,              KC_K,       KC_H,   KC_COMM, KC_DOT,     KC_SLSH,    KC_LSFT,
-    KC_LCMD, TG(_GAME), _______, KC_LCMD, MO(_RAISE), SFT_T(KC_ESC),     KC_SPC, MO(_LOWER), KC_LALT, MO(_LOWER), MO(_RAISE), _______
-),
+    [_BASE] = LAYOUT_ortho_5x12(
+        KC_EQL,  KC_1,      KC_2,    KC_3,    KC_4,       KC_5,                       KC_6,       KC_7,       KC_8,    KC_9,       KC_0,       KC_MINS,
+        KC_TAB,  KC_Q,      KC_W,    KC_F,    KC_P,       KC_B,                       KC_J,       KC_L,       KC_U,    KC_Y,       KC_QUOT,    KC_GRV,
+        KC_BSLS, KC_A,      KC_R,    KC_S,    KC_T,       KC_G,                       KC_M,       KC_N,       KC_E,    KC_I,       KC_O,       KC_SCLN,
+        KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_D,       KC_V,                       KC_K,       KC_H,       KC_COMM, KC_DOT,     KC_SLSH,    KC_RSFT,
+        XXXXXXX, TG(_GAME), _______, MO(_RAISE), LGUI_T(KC_ESC), LCTL_T(KC_BSPC),     KC_SPC, LCTL_T(KC_ENT), KC_LALT, MO(_LOWER), MO(_RAISE), XXXXXXX
+    ),
 
-[_LOWER] = LAYOUT_ortho_5x12(
-    _______, KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-    _______, KC_F11,    KC_F12,  _______, _______, KC_LPRN,        KC_RPRN, KC_AMPR, KC_ASTR, KC_TILD, KC_GRV,  _______,
-    _______, KC_COLON,  KC_UNDS, KC_MINS, KC_EQL,  KC_LBRC,        KC_RBRC, KC_DLR,  KC_PERC, KC_CIRC, KC_PIPE, _______,
-    _______, KC_SCLN,   _______, _______, KC_PLUS, KC_LCBR,        KC_RCBR, KC_EXLM, KC_AT,   KC_HASH, KC_BSLS, _______,
-    _______, _______,   _______, _______, _______, _______,        _______, _______, _______, _______, _______, QK_BOOT
-),
+    [_LOWER] = LAYOUT_ortho_5x12(
+        KC_F12,  KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        _______, _______,   _______, _______, _______, _______,        _______, KC_AMPR, KC_ASTR, KC_TILD, KC_GRV,  _______,
+        KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, _______, _______,        _______, KC_DLR,  KC_PERC, KC_CIRC, KC_PIPE, _______,
+        KC_HOME, KC_PGDN,   KC_PGUP, KC_END,  _______, _______,        _______, KC_EXLM, KC_AT,   KC_HASH, KC_BSLS, _______,
+        _______, _______,   _______, _______, _______, _______,        _______, _______, _______, XXXXXXX, _______, QK_BOOT
+    ),
 
-[_RAISE] = LAYOUT_ortho_5x12(
-    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,        _______, KC_7,    KC_8,    KC_9,    _______, _______,
-    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,        KC_0,    KC_4,    KC_5,    KC_6,    KC_DOT,  _______,
-    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,        _______, KC_1,    KC_2,    KC_3,    KC_SLSH, _______,
-    _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, QK_BOOT
-),
+    [_RAISE] = LAYOUT_ortho_5x12(
+        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        _______, _______, _______, _______, _______, _______,        KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, _______,
+        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,        _______, KC_4,    KC_5,    KC_6,    KC_DOT,  _______,
+        KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,        _______, KC_1,    KC_2,    KC_3,    KC_SLSH, _______,
+        _______, _______, _______, XXXXXXX, _______, _______,        _______, KC_0, _______, _______,    _______, QK_BOOT
+    ),
 
-[_GAME] = LAYOUT_ortho_5x12(
-    KC_ESC,   KC_1,      KC_2,    KC_3,    KC_4,    KC_5,           KC_6,   KC_7,       KC_8,    KC_9,       KC_0,       KC_BSPC,
-    KC_TAB,   KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,   KC_U,       KC_I,    KC_O,       KC_P,       KC_DEL,
-    KC_LCTL,  KC_A,      KC_S,    KC_D,    KC_F,    KC_G,           KC_H,   KC_J,       KC_K,    KC_L,       KC_SCLN,    KC_ENT,
-    KC_LSFT,  KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,           KC_N,   KC_M,       KC_COMM, KC_DOT,     KC_SLSH,    KC_LSFT,
-    TG(_GAME),_______,   KC_M,    KC_LALT, KC_SPC,  KC_GRV,         KC_SPC, MO(_LOWER), KC_LCMD, MO(_LOWER), MO(_RAISE), _______
-)
+    [_GAME] = LAYOUT_ortho_5x12(
+        KC_ESC,   KC_1,      KC_2,    KC_3,    KC_4,    KC_5,           KC_6,   KC_7,       KC_8,    KC_9,       KC_0,       KC_BSPC,
+        KC_TAB,   KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,   KC_U,       KC_I,    KC_O,       KC_P,       KC_DEL,
+        KC_LCTL,  KC_A,      KC_S,    KC_D,    KC_F,    KC_G,           KC_H,   KC_J,       KC_K,    KC_L,       KC_SCLN,    KC_ENT,
+        KC_LSFT,  KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,           KC_N,   KC_M,       KC_COMM, KC_DOT,     KC_SLSH,    KC_LSFT,
+        TG(_GAME),_______,   KC_M,    KC_LALT, KC_SPC,  KC_GRV,         KC_SPC, MO(_LOWER), KC_LCMD, MO(_LOWER), MO(_RAISE), _______
+    )
 
 };
