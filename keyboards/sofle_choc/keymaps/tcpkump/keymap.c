f017c1fe43b17f014ac45da58da85c4abf4d6fc2
Garrett Leber <garrett@leberra.com>
2024-08-08 16:57:17 -0400

    improve sofle keymap

--- keyboards/sofle_choc/keymaps/tcpkump/keymap.c
+++ keyboards/sofle_choc/keymaps/tcpkump/keymap.c
@@ -30,12 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,           KC_Q,       KC_W,       KC_F,             KC_P,    KC_B,                                       KC_J,    KC_L,    KC_U,             KC_Y,      KC_QUOT, KC_GRV,
   KC_BSLS,          KC_A,       KC_R,       KC_S,             KC_T,    KC_G,                                       KC_M,    KC_N,    KC_E,             KC_I,      KC_O,    TD(TD_SCLN_COLN),
   TD(TD_LSFT_LPRN), KC_Z,       KC_X,       KC_C,             KC_D,    KC_V,    KC_MUTE,           KC_MPLY,        KC_K,    KC_H,    KC_COMM,          KC_DOT,    KC_SLSH, TD(TD_RSFT_RPRN),
-                                _______, TD(TD_LBRC_LCBR), KC_LCMD, KC_BSPC, LCTL_T(KC_ESC),    LCTL_T(KC_ENT), KC_SPC,  KC_LALT, TD(TD_RBRC_RCBR), TT(_LOWER)
+                                _______, TD(TD_LBRC_LCBR), KC_LCMD, LT(_LOWER,KC_BSPC), LCTL_T(KC_ESC),    LCTL_T(KC_ENT), KC_SPC,  KC_LALT, TD(TD_RBRC_RCBR), TT(_LOWER)
 ),
 [_LOWER] = LAYOUT(
   _______,          KC_F1,      KC_F2,      KC_F3,            KC_F4,   KC_F5,                                      KC_F6,   KC_F7,   KC_F8,            KC_F9,     KC_F10,  _______,
-  _______,          KC_F11,     KC_F12,     _______,          _______, _______,                                    _______, KC_7,    KC_8,             KC_9,      _______, _______,
-  _______,          KC_LEFT,    KC_DOWN,    KC_UP,            KC_RGHT, _______,                                    _______, KC_4,    KC_5,             KC_6,      KC_DOT,  _______,
+  KC_EQL,           KC_F11,     KC_F12,     _______,          _______, KC_LALT,                                    _______, KC_7,    KC_8,             KC_9,      _______, KC_MINS,
+  KC_LBRC,          KC_LEFT,    KC_DOWN,    KC_UP,            KC_RGHT, KC_LGUI,                                    _______, KC_4,    KC_5,             KC_6,      KC_DOT,  KC_RBRC,
   _______,          KC_HOME,    KC_PGDN,    KC_PGUP,          KC_END,  _______, _______,           _______,        KC_0,    KC_1,    KC_2,             KC_3,      KC_SLSH, _______,
                                 _______,    _______,          _______, _______, TG(_GAME),         _______,        _______, _______, _______,          _______
 ),
