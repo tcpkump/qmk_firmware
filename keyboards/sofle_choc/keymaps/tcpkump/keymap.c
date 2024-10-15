e2021977c945860e1d1fd2a31933cbe210543290
Garrett Leber <garrett.leber@openvpn.net>
2024-10-15 12:47:19 -0400

    Update keymaps

--- keyboards/sofle_choc/keymaps/tcpkump/keymap.c
+++ keyboards/sofle_choc/keymaps/tcpkump/keymap.c
@@ -12,10 +12,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,           KC_Q,       KC_W,       KC_F,             KC_P,    KC_B,                                       KC_J,    KC_L,    KC_U,             KC_Y,      KC_QUOT, KC_GRV,
   KC_BSLS,          KC_A,       KC_R,       KC_S,             KC_T,    KC_G,                                       KC_M,    KC_N,    KC_E,             KC_I,      KC_O,    KC_SCLN,
   KC_LSFT,          KC_Z,       KC_X,       KC_C,             KC_D,    KC_V,    KC_MUTE,           KC_MPLY,        KC_K,    KC_H,    KC_COMM,          KC_DOT,    KC_SLSH, KC_RSFT,
-                                _______,    _______, KC_LCMD, LT(_LOWER,KC_BSPC), LCTL_T(KC_ESC),    LCTL_T(KC_ENT), KC_SPC,  KC_LALT, _______,        MO(_LOWER)
+                                KC_LEFT,    KC_RGHT, KC_LCMD, LT(_LOWER,KC_BSPC), LCTL_T(KC_ESC),    LCTL_T(KC_ENT), KC_SPC,  KC_LALT, KC_DOWN,        KC_UP
 ),
 [_LOWER] = LAYOUT(
-  _______,          KC_F1,      KC_F2,      KC_F3,            KC_F4,   KC_F5,                                      KC_F6,   KC_F7,   KC_F8,            KC_F9,     KC_F10,  _______,
+  _______,          KC_F1,      KC_F2,      KC_F3,            KC_F4,   KC_F5,                                      KC_F6,   KC_F7,   KC_F8,            KC_F9,     KC_F10,  KC_DEL,
   KC_EQL,           KC_F11,     KC_F12,     _______,          _______, KC_LALT,                                    _______, KC_7,    KC_8,             KC_9,      _______, KC_MINS,
   KC_LBRC,          KC_LEFT,    KC_DOWN,    KC_UP,            KC_RGHT, KC_LGUI,                                    _______, KC_4,    KC_5,             KC_6,      KC_DOT,  KC_RBRC,
   _______,          KC_HOME,    KC_PGDN,    KC_PGUP,          KC_END,  _______, _______,           _______,        KC_0,    KC_1,    KC_2,             KC_3,      KC_SLSH, _______,
