ff12ad4a11552aceafd2d14ae3f0d8c37a1a6624
Garrett Leber <garrett@leberra.com>
2025-03-17 21:01:42 -0400

    dactyl cc bottom row mods (36 key pilot)
    Piloting a 36 key layout to see how I might like it

--- keyboards/handwired/dactyl_cc_tcpkump/keymaps/default/config.h
+++ keyboards/handwired/dactyl_cc_tcpkump/keymaps/default/config.h
@@ -3,22 +3,6 @@
 
 #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* Enables This makes it easier for fast typists to use dual-function keys */
-#define PERMISSIVE_HOLD
-
-/* mouse config */
-#define MOUSEKEY_DELAY          0
-// Lowering MOUSEKEY_INTERVAL raises the cursor speed significantly, you may want to lower MOUSEKEY_MAX_SPEED (default: 10)
-#define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_MAX_SPEED       7
-#define MOUSEKEY_WHEEL_DELAY 0
-
 /* layer config */
 #define TAPPING_TOGGLE 1
 
@@ -30,3 +14,10 @@
 /* tcpkump */
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Home Row Mods
+#define TAPPING_TERM 180
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
--- keyboards/handwired/dactyl_cc_tcpkump/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_cc_tcpkump/keymaps/default/keymap.c
@@ -5,30 +5,78 @@
 
 enum layers {
     BASE,
-    LOWER
+    LOWER,
+    UPPER,
+    ADJUST,
+    FUNC
 };
 
 #define LT_BSPC LT(LOWER, KC_BSPC)
+#define LT_SPC  LT(UPPER, KC_SPC)
+#define LT_ENT  LT(FUNC,  KC_ENT)
+
+// Left-hand home row mods
+#define HOME_Z LGUI_T(KC_Z)
+#define HOME_X LALT_T(KC_X)
+#define HOME_C LSFT_T(KC_C)
+#define HOME_D LCTL_T(KC_D)
+
+// Right-hand home row mods
+#define HOME_H    RCTL_T(KC_H)
+#define HOME_COMM RSFT_T(KC_COMM)
+#define HOME_DOT  LALT_T(KC_DOT)
+#define HOME_SLSH RGUI_T(KC_SLSH)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_EQL,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                                         KC_6, KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,   KC_F,    KC_P,    KC_B,                                         KC_J, KC_L,    KC_U,    KC_Y,   KC_QUOT, KC_GRV,
-        KC_CAPS, KC_A,    KC_R,   KC_S,    KC_T,    KC_G,                                         KC_M, KC_N,    KC_E,    KC_I,   KC_O,    KC_SCLN,
-        KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_D,    KC_V,                                         KC_K, KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,                                                     KC_DEL, KC_BSLS, KC_LBRC,  KC_RBRC,
-                                                          KC_END,  KC_ESC,     LT_BSPC, KC_HOME,
-                                                 KC_LCTL, KC_LGUI, KC_LALT,     KC_RCTL, KC_PGUP,  KC_SPC,
-                                                                   KC_ENT,     KC_PGDN
+        _______, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______,   _______,
+        _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                         KC_J, KC_L,    KC_U,      KC_Y,     KC_QUOT,   _______,
+        _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                         KC_M, KC_N,    KC_E,      KC_I,     KC_O,      _______,
+        _______, HOME_Z,  HOME_X,  HOME_C,  HOME_D,  KC_V,                                         KC_K, HOME_H,  HOME_COMM, HOME_DOT, HOME_SLSH, _______,
+                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,                                                     KC_DEL,  KC_BSLS,   KC_LBRC,  KC_RBRC,
+                                                          _______, KC_ESC,      LT_BSPC, _______,
+                                                 KC_TAB,  _______, _______,     _______, _______,  LT_SPC,
+                                                                   LT_ENT,      _______
     ),
     [LOWER] = LAYOUT(
-        KC_F1,   KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,                                        KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        QK_BOOT, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
+        QK_BOOT, _______, _______, _______, _______, _______,                                      KC_PLUS, KC_7,    KC_8,    KC_9,    KC_DOT,  _______,
+        _______, _______, _______, _______, _______, _______,                                      KC_EQL,  KC_4,    KC_5,    KC_6,    KC_0,    _______,
+        _______, _______, _______, _______, _______, _______,                                      KC_MINS, KC_1,    KC_2,    KC_3,    KC_ASTR, _______,
+                 _______, _______, _______, _______,                                                        _______, _______, _______, _______,
+                                                          _______, KC_ESC,      LT_BSPC, _______,
+                                                 KC_TAB,  _______, _______,     _______, _______,  MO(ADJUST),
+                                                                   LT_ENT,      _______
+    ),
+    [UPPER] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
-                 _______, _______, _______, _______,                                                    _______, _______, _______, _______,
-                                                          _______, _______, _______, _______,
-                                                 _______, _______, _______, _______, _______, _______,
-                                                                   _______, _______
+        QK_BOOT, KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_PIPE,                                      _______, _______, _______, _______, _______, _______,
+        _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                       _______, _______, _______, _______, _______, _______,
+        _______, KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_TILD,                                      _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______,                                                         _______, _______, _______, _______,
+                                                          _______, KC_ESC,      MO(ADJUST), _______,
+                                                 KC_TAB,  _______, _______,     _______, _______,  LT_SPC,
+                                                                   LT_ENT,      _______
+    ),
+    [ADJUST] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+        QK_BOOT, KC_AMPR, KC_UNDS, KC_BSLS, KC_SCLN, _______,                                       _______, _______, _______, _______, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,                                       _______, _______, _______, _______, _______, _______,
+        _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,                                       _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______,                                                         _______, _______, _______, _______,
+                                                          _______, KC_ESC,      LT_BSPC, _______,
+                                                 KC_TAB,  _______, _______,     _______, _______,  LT_SPC,
+                                                                   LT_ENT,      _______
+    ),
+    [FUNC] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, KC_F11,  KC_F12,  _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______,                                                         _______, _______, _______, _______,
+                                                          _______, KC_ESC,      LT_BSPC, _______,
+                                                 KC_TAB,  _______, _______,     _______, _______,  LT_SPC,
+                                                                   LT_ENT,      _______
     )
 };
